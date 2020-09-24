44cf89a585e7703ce185c7fae54013264ee739bc
J0NY97 <jony.salmi97@gmail.com>
2020-09-24 13:27:55 +0300

    small fix in memalloc.c

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: jsalmi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/17 10:06:57 by jsalmi            #+#    #+#             */
-/*   Updated: 2019/11/07 12:54:01 by jsalmi           ###   ########.fr       */
+/*   Updated: 2020/09/24 13:27:28 by jsalmi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	*ft_memalloc(size_t size)
 {
 	char *this;
 
-	if (!(this = (void *)malloc(sizeof(size_t) * size)))
+	if (!(this = (void *)malloc(size)))
 		return (0);
 	ft_bzero(this, size);
 	return (this);
