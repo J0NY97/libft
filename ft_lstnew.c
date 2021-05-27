be10a4e34bdca6d6ee668079c23bfa247e01df68
J0NY97 <jony.salmi97@gmail.com>
2021-05-27 18:15:25 +0300

    list has now prev, and edited some functions

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -12,6 +12,7 @@
 
 #include "./libft.h"
 
+/*
 t_list	*ft_lstnew(void const *content, size_t content_size)
 {
 	t_list	*node;
@@ -32,4 +33,29 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 	}
 	node->next = NULL;
 	return (node);
+}
+*/
+t_list	*ft_lstnew(void const *content, size_t content_size)
+{
+	t_list	*new;
+
+	new = (t_list *)ft_memalloc(sizeof(*new));
+	if (!new)
+		return (0);
+	if (content == NULL)
+	{
+		new->content = NULL;
+		new->content_size = 0;
+		new->next = NULL;
+		new->prev = NULL;
+		return (new);
+	}
+	new->content = malloc(content_size);
+	if (!new->content)
+		return (0);
+	ft_memmove(new->content, content, content_size);
+	new->content_size = content_size;
+	new->next = NULL;
+	new->prev = NULL;
+	return (new);
 }
